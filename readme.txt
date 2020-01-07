4f5d1ecca2fe9af46b303fabe69fb770fa47f07c
student <jwang@vsu.edu>
2020-01-07 02:27:51 -0500

    train.py gpu memory bug fixed by set batch_size=4 for 2nd phase, msi laptop

--- readme.txt
+++ readme.txt
@@ -1,10 +1,28 @@
 
+-----------------1/7/2020 training from a previous step--------
+msi, sda19, tf14-gpu
+python3 train.py --weights_file logs/000/trained_weights_stage_1.h5 --phase_i 2
+python3 train.py --weights_file logs/000/trained_weights_final.h5 --phase_i 2
+
+	-phase_i : number of epoch for phase i, default 50
+	-weights_file: initial weights
+
+run out of memory at phase 2, set 
+	batch_size=4 ok
+
+train rst glance:
+	562/562 [==============================] - 318s 566ms/step - loss: 31.3958 - val_loss: 18.2914
+	Epoch 52/100
+	562/562 [==============================] - 314s 559ms/step - loss: 28.4765 - val_loss: 25.5125
+
+-----------------1/6/2020 training steps------------------------
 If you want to use original pretrained weights for YOLOv3:
 0. python3 voc_annotation.py , this create annotate data file from VOCdevkit 
 1. wget https://pjreddie.com/media/files/darknet53.conv.74
 2. rename it as darknet53.weights
 3. python3 convert.py -w darknet53.cfg darknet53.weights model_data/darknet53_weights.h5
 4. use model_data/darknet53_weights.h5 in train.py
+5. logs/000$ tensorboard --logdir .
 
 trainging results:
 	log/000/
--- train.py
+++ train.py
@@ -1,7 +1,15 @@
 """
 Retrain the YOLO model for your own dataset.
 """
+#import tensorflow as tf
+#from tensorflow.compat.v1 import ConfigProto
+#from tensorflow.compat.v1 import InteractiveSession
 
+#config = tf.ConfigProto()
+#config.gpu_options.allow_growth = True
+#session = tf.InteractiveSession(config=config)
+
+import tensorflow as tf
 import numpy as np
 import keras.backend as K
 from keras.layers import Input, Lambda
@@ -11,6 +19,11 @@ from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, Ear
 
 from yolo3.model import preprocess_true_boxes, yolo_body, tiny_yolo_body, yolo_loss
 from yolo3.utils import get_random_data
+FLAGS = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_string(
+    'weights_file', '', 'Binary file with detector weights')
+tf.app.flags.DEFINE_integer(
+    'phase_i', 50, '50')
 
 
 def _main():
@@ -23,18 +36,24 @@ def _main():
     anchors = get_anchors(anchors_path)
 
     input_shape = (416,416) # multiple of 32, hw
-
+    print('flag.weights_file ', FLAGS.weights_file)
+    #return
     is_tiny_version = len(anchors)==6 # default setting
     if is_tiny_version:
         model = create_tiny_model(input_shape, anchors, num_classes,
             freeze_body=2, weights_path='model_data/tiny_yolo_weights.h5')
     else:
+      if FLAGS.weights_file=='':
         model = create_model(input_shape, anchors, num_classes,
             freeze_body=2, weights_path='model_data/darknet53_weights.h5') # make sure you know what you freeze
+      else:
+        model = create_model(input_shape, anchors, num_classes,
+            freeze_body=2, weights_path=FLAGS.weights_file) # continue train from a provided h5 file
+
 
     logging = TensorBoard(log_dir=log_dir)
     checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
-        monitor='val_loss', save_weights_only=True, save_best_only=True, period=3)
+        monitor='val_loss', save_weights_only=True, save_best_only=True, period=15)
     reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, verbose=1)
     early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1)
 
@@ -60,7 +79,7 @@ def _main():
                 steps_per_epoch=max(1, num_train//batch_size),
                 validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
                 validation_steps=max(1, num_val//batch_size),
-                epochs=50,
+                epochs=FLAGS.phase_i,
                 initial_epoch=0,
                 callbacks=[logging, checkpoint])
         model.save_weights(log_dir + 'trained_weights_stage_1.h5')
@@ -73,7 +92,7 @@ def _main():
         model.compile(optimizer=Adam(lr=1e-4), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
         print('Unfreeze all of the layers.')
 
-        batch_size = 32 # note that more GPU memory is required after unfreezing the body
+        batch_size = 4 # note that more GPU memory is required after unfreezing the body
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
         model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
             steps_per_epoch=max(1, num_train//batch_size),
